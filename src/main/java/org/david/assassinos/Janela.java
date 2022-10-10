eb8762b7f44d6992fe7e58a38af0bfc5dbe11fd5
João Ricardo <91494443+JOHN-RlCHARD@users.noreply.github.com>
2022-10-10 11:36:51 -0300

    first GUI configurations

--- src/main/java/org/david/assassinos/Janela.java
+++ src/main/java/org/david/assassinos/Janela.java
@@ -1,14 +1,43 @@
 package org.david.assassinos;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.FlowLayout;
 import javax.swing.*;
-
 public class Janela extends JFrame {
     protected static final long serialVersionUID = 1L;
 
-    protected JButton btnPonto = new JButton("Ponto");
+    protected JButton btnLimpar = new JButton("Limpar");
+    protected JButton btnOk = new JButton("Ok");
 
+    private JTextField fixa, caixa;
     public Janela() {
         super("Assassinos e Vítimas");
+        setLayout( new FlowLayout() );
+
+        fixa = new JTextField("Nome do Assassino:");
+        fixa.setEditable(false);
+        add(fixa);
+
+        caixa=new JTextField(20);
+        add(caixa);
+
+        btnOk.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if(e.getSource() == btnOk)
+                    JOptionPane.showMessageDialog(null, caixa.getText());
+            }
+        });
+        btnLimpar.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if(e.getSource() == btnLimpar)
+                    caixa.setText("");
+            }
+        });
+        add(btnLimpar);
+        add(btnOk);
 
         this.setSize(800, 600);
         this.setVisible(true);
